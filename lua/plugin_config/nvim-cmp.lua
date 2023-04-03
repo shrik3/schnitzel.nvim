0fdbe1c836f959c98b4979376793fcc1d36b96e5
Tianhao Wang <shrik3@riseup.net>
2023-04-04 01:57:22 +0200

    update configs...

--- lua/plugin_config/nvim-cmp.lua
+++ lua/plugin_config/nvim-cmp.lua
@@ -6,8 +6,9 @@ keymap("i", "<C-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
 keymap("s", "<C-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
 -- Set up nvim-cmp.
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 local cmp = require'cmp'
 local luasnip = require'luasnip'
