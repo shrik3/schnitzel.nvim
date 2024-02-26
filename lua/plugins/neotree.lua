d419b4750a77dbea6ae2e658443068c8a70ab02a
Tianhao Wang <wth@riseup.net>
2024-02-26 14:37:21 +0100

    keybinding for buffer list and git

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -12,8 +12,9 @@ M.config = function()
 	vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 	vim.keymap.set("n", "<F10>", ":Neotree buffers float toggle<CR>")
 	vim.keymap.set("n", "<leader>bb", ":Neotree buffers toggle<CR>")
-	vim.keymap.set("n", "<leader>bg", ":Neotree git_status toggle<CR>")
 	vim.keymap.set("n", "<F9>", ":Neotree toggle<CR>")
+	vim.keymap.set("n", "<leader>tt", ":Neotree float toggle<CR>")
+	vim.keymap.set("n", "<leader>tg", ":Neotree git_status float toggle<CR>")
 	require("neo-tree").setup({})
 end
 
