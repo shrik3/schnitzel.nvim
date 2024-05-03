cf85ca06fca57e5fea3cc2caf640b687595b2e10
Tianhao Wang <shrik3@mailbox.org>
2024-05-03 04:18:20 +0200

    tweak mappings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,7 +2,6 @@ local M = {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
 		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-nvim-lua",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
@@ -76,9 +75,7 @@ M.config = function()
 		}),
 		sources = cmp.config.sources({
 			{ name = "nvim_lsp" },
-			{ name = "nvim_lua" },
-			{ name = "luasnip" }, -- For luasnip users.
-			-- { name = "orgmode" },
+			{ name = "luasnip" },
 		}, {
 			{ name = "buffer" },
 			{ name = "path" },
