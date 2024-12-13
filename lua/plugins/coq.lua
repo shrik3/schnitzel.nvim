e81c3985427149d2411dd813afd43ba7b47bf324
Tianhao Wang <shrik3@mailbox.org>
2024-12-13 13:44:36 +0100

    plugins: add COQ for completion
    Signed-off-by: Tianhao Wang <shrik3@mailbox.org>

---
+++ lua/plugins/coq.lua
@@ -0,0 +1,48 @@
+local COQ = {
+	"ms-jpq/coq_nvim",
+	branch = "coq",
+	lazy = false,
+	dependencies = {
+		{ "ms-jpq/coq.thirdparty", branch = "3p" },
+		-- { "ms-jpq/coq.artifacts", branch = "artifacts" },
+	},
+}
+
+COQ.init = function()
+	vim.g.coq_settings = {
+		auto_start = "shut-up",
+		-- I don't want your default keymaps
+		keymap = {
+			recommended = false,
+			jump_to_mark = "",
+			bigger_preview = "",
+		},
+		display = {
+			ghost_text = { highlight_group = "Normal" },
+			icons = { mode = "none" },
+		},
+	}
+	vim.g.coq_settings.display = {
+		ghost_text = { enabled = false },
+	}
+	vim.api.nvim_set_keymap("i", "<Esc>", [[pumvisible() ? "\<C-e><Esc>" : "\<Esc>"]], { expr = true, silent = true })
+	vim.api.nvim_set_keymap("i", "<BS>", [[pumvisible() ? "\<C-e><BS>" : "\<BS>"]], { expr = true, silent = true })
+	vim.api.nvim_set_keymap("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], { expr = true, silent = true })
+	vim.api.nvim_set_keymap("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<BS>"]], { expr = true, silent = true })
+	vim.api.nvim_set_keymap(
+		"i",
+		"<CR>",
+		[[pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"]],
+		{ expr = true, silent = true }
+	)
+	require("coq_3p")({
+		-- { src = "dap" },
+		-- { src = "builtin/js" },
+		-- { src = "builtin/php" },
+		{ src = "builtin/html", short_name },
+		{ src = "builtin/css" },
+		{ src = "builtin/c" },
+	})
+end
+
+return COQ
