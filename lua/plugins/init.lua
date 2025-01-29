652ccd760b4ea77a4cf60cfb7daa7975cd731aec
Tianhao Wang <shrik3@mailbox.org>
2025-01-29 09:38:19 +0100

    chores
    Signed-off-by: Tianhao Wang <shrik3@mailbox.org>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,22 +1,14 @@
 return {
 	"shrik3/dagon",
-	-- tagbar shows a bar of tags
 	"ntpeters/vim-better-whitespace",
 	"godlygeek/tabular",
-	-- split VeryLongWords for easier w nav
 	"chaoren/vim-wordmotion",
-	-- harelang support
+
 	{ name = "hare.vim", url = "https://git.sr.ht/~sircmpwn/hare.vim" },
-	-- AspectC++ highlighting
 	"shrik3/vim-aspectcpp",
-	-- aarch64 assembly syntax hl
 	"compnerd/arm64asm-vim",
-	-- copy paste images
-	{ "HakonHarnes/img-clip.nvim", event = "VeryLazy" },
-	-- other preconfigured plugins
-	require("plugins.preconfigured.autopairs"),
-	require("plugins.preconfigured.bookmarks"),
-	require("plugins.preconfigured.colorizer"),
+
+	-- preconfigured
 	require("plugins.preconfigured.comment"),
 	require("plugins.preconfigured.format"),
 	require("plugins.preconfigured.git"),
@@ -24,9 +16,15 @@ return {
 	require("plugins.preconfigured.lspconfig"),
 	require("plugins.preconfigured.markdown"),
 	require("plugins.preconfigured.neotree"),
-	-- completion engine, coq and cmp are not compatible
 	require("plugins.preconfigured.rust"),
 	require "plugins.preconfigured.nvim-cmp",
 	require("plugins.preconfigured.telescope"),
 	require("plugins.preconfigured.persistence"),
+	require("plugins.preconfigured.autopairs"),
+	require("plugins.preconfigured.bookmarks"),
+	require("plugins.preconfigured.colorizer"),
+	require("plugins.preconfigured.paste-image"),
+
+	-- inactive - preconfigured
+	-- require("plugins.preconfigured.surround"),
 }
