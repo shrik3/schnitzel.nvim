b4ae30ba949920d3564c76a3035475a09bd44220
Tianhao Wang <shrik3@mailbox.org>
2025-02-02 04:36:30 +0100

    docs: add more comments
    Signed-off-by: Tianhao Wang <shrik3@mailbox.org>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,31 +1,59 @@
 return {
+    -- colorscheme
 	"shrik3/dagon",
+    -- no trailing white spaces
 	"ntpeters/vim-better-whitespace",
+    -- better word motion (press w)
 	"chaoren/vim-wordmotion",
 	-- programming
 
 	-- preconfigured
 	require("plugins.preconfigured.comment"),
+    -- auto code formatting
 	require("plugins.preconfigured.format"),
+    -- git integration (GitSigns)
 	require("plugins.preconfigured.git"),
+    -- ctag integration
 	require("plugins.preconfigured.tagbar"),
+    -- language servers
 	require("plugins.preconfigured.lspconfig"),
+    -- markdown preview and helpers
 	require("plugins.preconfigured.markdown"),
+    -- file tree navigator
 	require("plugins.preconfigured.neotree"),
+    -- completion engine
 	require("plugins.preconfigured.nvim-cmp"),
+    -- all in one searching UI
 	require("plugins.preconfigured.telescope"),
+    -- auto session management
 	require("plugins.preconfigured.persistence"),
 
 	-- inactive
+
+    -- making plain text tables
 	-- "godlygeek/tabular",
+
+    -- language: hare
 	-- { name = "hare.vim", url = "https://git.sr.ht/~sircmpwn/hare.vim" },
+
+    -- language: aspect C++
 	-- "shrik3/vim-aspectcpp",
+
+    -- language: arm64 assembly
 	-- "compnerd/arm64asm-vim",
 
-	-- inactive - preconfigured
-	-- require("plugins.preconfigured.paste-image"),
-	-- require("plugins.preconfigured.autopairs"),
-	-- require("plugins.preconfigured.surround"),
-	-- require("plugins.preconfigured.colorizer"),
+    -- language: rust
 	-- require("plugins.preconfigured.rust"),
+
+    -- "paste" image from clipboard
+	-- require("plugins.preconfigured.paste-image"),
+
+    -- auto closing brackets ... do you really need it?
+	-- require("plugins.preconfigured.autopairs"),
+
+    -- well. I tried it and it feels more bloat than convenient.
+	-- require("plugins.preconfigured.surround"),
+
+    -- show css hex colors
+	-- require("plugins.preconfigured.colorizer"),
 }
